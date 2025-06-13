8c82841d3415d7f7b875dde4ef62074365c0dd94
mitessier <mitessier@maisonsdumonde.com>
2025-06-13 10:50:45 +0200

    init apollo-client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,21 +8,21 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen justify-between bg-gray-100">
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen justify-between bg-gray-100">
 
-          <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* <Route path="/login" element={<Login />} /> */}
-              {/* <Route path="/register" element={<Register />} /> */}
-              {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
-              {/* <Route path="/me/bookings" element={<MyBookings />} /> */}
-              {/* <Route path="/me/profile" element={<UserProfile />} /> */}
-            </Routes>
-          <Footer />
-        </div>
-      </BrowserRouter>
+            <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/login" element={<Login />} /> */}
+                {/* <Route path="/register" element={<Register />} /> */}
+                {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+                {/* <Route path="/me/bookings" element={<MyBookings />} /> */}
+                {/* <Route path="/me/profile" element={<UserProfile />} /> */}
+              </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
     </>
   )
 }

73d4fa13560203c54d159178579209dbff02af66
mitessier <mitessier@maisonsdumonde.com>
2025-07-05 21:15:08 +0200

    add register view

--- client/codegen.ts
+++ client/codegen.ts
@@ -5,8 +5,24 @@ const config: CodegenConfig = {
   documents: ["src/**/*.graphql", "src/**/*.queries.ts"],
   generates: {
     './src/graphql/__generated__/types.ts': {
-      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo']
-    }
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      config: {
+        scalars: {
+          EmailAddress: {
+            input: 'string', 
+            output: 'string', 
+          },
+          ObjectID: {
+            input: 'string', 
+            output: 'string', 
+          },
+          DateTime: {
+            input: 'Date', 
+            output: 'Date', 
+          },
+        }
+      }
+    },
   },  
   ignoreNoDocuments: true,  
 };
---
+++ client/src/components/pages/Auth/Register.tsx
@@ -0,0 +1,155 @@
+import { Link } from "react-router-dom";
+import { Button } from "@/components/shadcn/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcn/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  // FormMessage,
+} from "@/components/shadcn/form";
+import { Input } from "@/components/shadcn/input";
+import { MailPlus } from "lucide-react";
+
+import { useForm } from "react-hook-form"
+import { useRegisterUserMutation } from "@/graphql/__generated__/types";
+import Loader from "@/components/ui/Loader";
+import { toast } from "sonner"
+
+const Register = () => {
+  const form = useForm();
+
+  const [registerUserMutation, { loading} ] = useRegisterUserMutation()
+
+
+  async function onSubmit(payload: any) {
+
+    await registerUserMutation({
+      variables: {
+        input: {
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          email: payload.email,
+          password: payload.password,
+        }
+      },
+      onError: () => {
+        toast.error(`Error during registration`);
+      },
+      onCompleted: () => {
+        toast.success("Registration successful! You can now log in.");
+        // Redirect to login or home page
+      }
+    })
+  
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex min-h-screen flex-1 flex-col gap-4 bg-muted/40 justify-center">
+          <Card className="mx-auto w-full max-w-lg shadow-lg">
+            <CardHeader>
+              <CardTitle className="text-2xl">Sign Up</CardTitle>
+              <CardDescription>
+                Enter your information to create an account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <FormField 
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your first name"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField 
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Last Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your last name"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField 
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your email"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField 
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="Enter your password"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  <div className="h-4 w-4 ml-2" >
+                    { loading ? <Loader  /> : <MailPlus /> }
+                  </div>
+                  Sign Up 
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link to="/login" className="underline">
+                  Sign in
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </form>
+    </Form>
+
+  );
+};
+
+export default Register;

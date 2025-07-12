bfc729600bc6f06983c3c8f511e1f8cb925d3342
mitessier <mitessier@maisonsdumonde.com>
2025-07-12 21:34:15 +0200

    add guard permissions

--- server/api/middlewares/permissions.middleware.ts
+++ server/api/middlewares/permissions.middleware.ts
@@ -1,3 +1,17 @@
-import { shield } from 'graphql-shield';
+import { Context } from '@/apollo/context';
+import { rule, shield } from 'graphql-shield';
+import { UserRole } from '../__generated__/graphql';
 
-export const permissions = shield({})
+const isAuthenticated = rule({ cache: 'contextual' })((_parent, _args, context: Context) => {
+  return context.user !== null;
+})
+
+const isAdmin = rule({ cache: 'contextual' })((parent, args, context: Context) => {
+  return Boolean(context.user?.role?.includes(UserRole.Admin))
+})
+
+export const permissions = shield({
+  Query: {
+    me: isAuthenticated
+  }
+})
